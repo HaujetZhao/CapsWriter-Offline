ce0b5fd522215f4a918fc668c0f918103e8513d1
Haujet <1292756898@qq.com>
2024-01-12 15:23:42 +0800

    尝试用上下文管理器处理错误

--- util/client_check_websocket.py
+++ util/client_check_websocket.py
@@ -4,19 +4,40 @@ from util.client_cosmic import Cosmic, console
 from config import ClientConfig as Config
 
 
+class Handler:
+    def __enter__(self):...
+
+    def __exit__(self, exc_type, e, exc_tb):
+        if isinstance(e, ConnectionRefusedError):
+            return True
+        elif isinstance(e, TimeoutError):
+            return True
+        elif isinstance(e, Exception):
+            return True
+        else:
+            print(e)
+
+
 async def check_websocket() -> bool:
     if Cosmic.websocket and not Cosmic.websocket.closed:
         return True
     for _ in range(3):
-        try:
+        with Handler():
             Cosmic.websocket = await websockets.connect(f"ws://{Config.addr}:{Config.port}", max_size=None)
             return True
-        except ConnectionRefusedError as e:
-            continue
-        except TimeoutError:
-            continue
-        except Exception as e:
-            print(e)
-
     else:
         return False
+
+    # for _ in range(3):
+    #     try:
+    #         Cosmic.websocket = await websockets.connect(f"ws://{Config.addr}:{Config.port}", max_size=None)
+    #         return True
+    #     except ConnectionRefusedError as e:
+    #         continue
+    #     except TimeoutError:
+    #         continue
+    #     except Exception as e:
+    #         print(e)
+    #
+    # else:
+    #     return False
