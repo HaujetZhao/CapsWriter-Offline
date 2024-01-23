e9e0169e30b2ba541ddf7d859efee21e203f31a4
H1DDENADM1N <sudoomg@outlook.com>
2024-01-23 22:17:32 +0800

    安装ffmpeg后输出mp3弹窗影响打字输入，去掉这段继续wav保存
    非GUI命令行运行core_client有同样bug

--- util/client_create_file.py
+++ util/client_create_file.py
@@ -20,21 +20,21 @@ def create_file(channels: int, time_start: float) -> Tuple[Path, Union[Popen, Wa
     file_path = tempfile.mktemp(prefix=f'({time_ymdhms})', dir=folder_path)
     file_path = Path(file_path)
 
-    if shutil.which('ffmpeg'):
-        # 用户已安装 ffmpeg，则输出到 mp3 文件
-        file_path = file_path.with_suffix('.mp3')
-        # 构造ffmpeg命令行
-        ffmpeg_command = [
-            'ffmpeg', '-y', 
-            '-f', 'f32le', '-ar', '48000', '-ac', f'{channels}', '-i', '-',
-            '-b:a', '192k', file_path,
-        ]
-        # 执行ffmpeg命令行，得到 Popen
-        file = Popen(ffmpeg_command, stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL)
-    else:                       # 用户未安装 ffmpeg，则输出为 wav 格式
-        file_path = file_path.with_suffix('.wav')
-        file = wave.open(str(file_path), 'w')
-        file.setnchannels(channels)
-        file.setsampwidth(2)
-        file.setframerate(48000)
+    # if shutil.which('ffmpeg'):
+    #     # 用户已安装 ffmpeg，则输出到 mp3 文件
+    #     file_path = file_path.with_suffix('.mp3')
+    #     # 构造ffmpeg命令行
+    #     ffmpeg_command = [
+    #         'ffmpeg', '-y', 
+    #         '-f', 'f32le', '-ar', '48000', '-ac', f'{channels}', '-i', '-',
+    #         '-b:a', '192k', file_path,
+    #     ]
+    #     # 执行ffmpeg命令行，得到 Popen
+    #     file = Popen(ffmpeg_command, stdin=PIPE, stdout=DEVNULL, stderr=DEVNULL)
+    # else:                       # 用户未安装 ffmpeg，则输出为 wav 格式
+    file_path = file_path.with_suffix('.wav')
+    file = wave.open(str(file_path), 'w')
+    file.setnchannels(channels)
+    file.setsampwidth(2)
+    file.setframerate(48000)
     return file_path, file
