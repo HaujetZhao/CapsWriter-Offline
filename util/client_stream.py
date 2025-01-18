98100e30725159b8853918e11127bec929b11932
PabloLION <36828324+PabloLION@users.noreply.github.com>
2025-01-18 07:24:43 +0100

    style: type client_stream

--- util/client_stream.py
+++ util/client_stream.py
@@ -2,15 +2,28 @@ import asyncio
 import sys
 import threading
 import time
+from types import FrameType
+from typing import Any, TypedDict, no_type_check
 
-import sounddevice as sd
+import sounddevice as sd  # pyright: ignore[reportMissingTypeStubs]
 
 from util.client_cosmic import ClientAppState, DataClientTask, console
 from util.types import RecordingData
 
 
+@no_type_check
+def restart_port_audio() -> None:
+    sd._terminate()
+    sd._ffi.dlclose(sd._lib)
+    sd._lib = sd._ffi.dlopen(sd._libname)
+    sd._initialize()
+
+
 def record_callback(
-    indata: RecordingData, _frames: int, _time_info, _status: sd.CallbackFlags
+    indata: RecordingData,
+    _frames: int,
+    _time_info: Any,
+    _status: sd.CallbackFlags,
 ) -> None:
     if not ClientAppState.on:
         return
@@ -28,35 +41,65 @@ def record_callback(
     )
 
 
-def stream_close(_signum: int, _frame: None) -> None:
+def stream_close(_signum: int, _frame: FrameType | None) -> None:
+    assert ClientAppState.stream is not None  # #TODO-REF-APP_CLASS: remove
     ClientAppState.stream.close()
 
 
-def stream_reopen():
+def stream_reopen() -> None:
     if not threading.main_thread().is_alive():
         return
     print("重启音频流")
 
     # 关闭旧流
+    assert ClientAppState.stream is not None  # #TODO-REF-APP_CLASS: remove
     ClientAppState.stream.close()
 
     # 重载 PortAudio，更新设备列表
-    sd._terminate()
-    sd._ffi.dlclose(sd._lib)
-    sd._lib = sd._ffi.dlopen(sd._libname)
-    sd._initialize()
+    restart_port_audio()
 
     # 打开新流
     time.sleep(0.1)
     ClientAppState.stream = stream_open()
 
 
+class DeviceDict(TypedDict):
+    """
+    # #TODO: Find stubfile for sounddevice
+    'name': name,
+    'index': device,
+    'hostapi': info.hostApi,
+    'max_input_channels': info.maxInputChannels,
+    'max_output_channels': info.maxOutputChannels,
+    'default_low_input_latency': info.defaultLowInputLatency,
+    'default_low_output_latency': info.defaultLowOutputLatency,
+    'default_high_input_latency': info.defaultHighInputLatency,
+    'default_high_output_latency': info.defaultHighOutputLatency,
+    'default_samplerate': info.defaultSampleRate,
+    """
+
+    name: str
+    index: int
+    hostapi: int
+    max_input_channels: int
+    max_output_channels: int
+    default_low_input_latency: float
+    default_low_output_latency: float
+    default_high_input_latency: float
+    default_high_output_latency: float
+    default_samplerate: float
+
+
 def stream_open() -> sd.InputStream:
     # 显示录音所用的音频设备
     channels = 1
     try:
-        device = sd.query_devices(kind="input")
-        channels = min(2, device["max_input_channels"])
+        device = DeviceDict(
+            sd.query_devices(  # pyright: ignore[reportArgumentType,reportUnknownMemberType]
+                kind="input"
+            )
+        )
+        channels = int(device["max_input_channels"])
         console.print(
             f'使用默认音频设备：[italic]{device["name"]}，声道数：{channels}',
             end="\n\n",
