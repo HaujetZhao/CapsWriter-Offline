02fc65a93018feca493017b0bc7325679e86dc03
Haujet Zhao <1292756898@qq.com>
2026-01-08 12:44:37 +0800

    清理

--- models/模型测试/02-02-Server 启动.bat
+++
@@ -1,8 +0,0 @@
-%~d0
-cd %~dp0
-
-if exist "model.int8.onnx" (
-    【连序转录】sherpa-onnx-offline-websocket-server.exe --paraformer=model.int8.onnx --tokens=tokens.txt --port=6007 --num-threads=8
-) else if exist "model_quant.onnx" (
-    【连序转录】sherpa-onnx-offline-websocket-server.exe --paraformer=model_quant.onnx --tokens=tokens.txt --port=6007 --num-threads=8
-) 
--- models/模型测试/readme.txt
+++
@@ -1,7 +0,0 @@
-00 用于把从 ModelScope 下载的 Paraformer-onnx 转为 Sherpa-onnx 可以使用的模型
-
-01 用于快速调用 Sherpa-onnx 和 Paraformer-onnx 转录音频文件，生成 txt json srt，但每次转录都需要载入一次模型文件
-
-02 用于启动 Sherpa-onnx 的 websocket 服务端，只需要载入一次模型文件，就可用客户端多次转录
-
-03 用于测试 funasr 的标点模型
