DF-wu/CapsWriter-Offline-Container

潜在热词获取修复

util/client/processing/result_processor.py
@@ -238,7 +238,7 @@ class ResultProcessor:
 
         # 热词替换
         # 1. 音素纠错
-        correction_result = self._hotword_manager.get_phoneme_corrector().correct(text)
+        correction_result = self._hotword_manager.get_phoneme_corrector().correct(text, k=10)
         text = correction_result.text
 
         # 2. 规则纠错
@@ -262,17 +262,21 @@ class ResultProcessor:
         if original_text_stripped != text:
             console.print(f'    热词替换：[cyan]{text}')
 
-        # 显示潜在热词列表（排除完全匹配的）
-        matched_hotwords = correction_result.matched_hotwords
-        if matched_hotwords:
-            # 过滤出完全匹配的热词（分数为 1.0）
-            exact_matches = [hw for hw, score in matched_hotwords if score >= 1.0]
-            # 过滤出潜在匹配的热词（分数小于 1.0）
-            potential_matches = [(hw, score) for hw, score in matched_hotwords if score < 1.0]
+        # 显示热词匹配情况
+        replaced_hotwords = correction_result.replaced_hotwords
+        context_hotwords = correction_result.potential_hotwords
 
-            if exact_matches:
-                console.print(f'    完全匹配：[green4]{", ".join(exact_matches)}')
+        # 1. 显示完全匹配/已替换的热词
+        if replaced_hotwords:
+            # 提取热词文本
+            replaced_names = [hw for hw, score in replaced_hotwords]
+            console.print(f'    完全匹配：[green4]{", ".join(replaced_names)}')
 
+        # 2. 显示潜在热词（从上下文热词中排除已替换的）
+        if context_hotwords:
+            replaced_set = {hw for hw, score in replaced_hotwords}
+            potential_matches = [(hw, score) for hw, score in context_hotwords if hw not in replaced_set]
+            
             if potential_matches:
                 # 格式化潜在匹配列表，显示分数
                 potential_str = ", ".join([f"{hw}({score:.2f})" for hw, score in potential_matches[:5]])
@@ -299,7 +303,7 @@ class ResultProcessor:
                 text,
                 return_result=True,
                 paste=paste,
-                matched_hotwords=correction_result.matched_hotwords
+                matched_hotwords=context_hotwords  # 传递上下文热词给 LLM
             )
         else:
             await self._text_output.output(text, paste=paste)

HaujetZhao/CapsWriter-Offline

util/client/processing/result_processor.py
@@ -238,7 +238,7 @@ class ResultProcessor:
 
         # 热词替换
         # 1. 音素纠错
-        correction_result = self._hotword_manager.get_phoneme_corrector().correct(text)
+        correction_result = self._hotword_manager.get_phoneme_corrector().correct(text, k=10)
         text = correction_result.text
 
         # 2. 规则纠错
@@ -262,17 +262,21 @@ class ResultProcessor:
         if original_text_stripped != text:
             console.print(f'    热词替换：[cyan]{text}')
 
-        # 显示潜在热词列表（排除完全匹配的）
-        matched_hotwords = correction_result.matched_hotwords
+        # 显示热词匹配情况
+        matched_hotwords = correction_result.matchs
+        potential_hotwords = correction_result.similars
+
+        # 1. 显示完全匹配/已替换的热词
         if matched_hotwords:
-            # 过滤出完全匹配的热词（分数为 1.0）
-            exact_matches = [hw for hw, score in matched_hotwords if score >= 1.0]
-            # 过滤出潜在匹配的热词（分数小于 1.0）
-            potential_matches = [(hw, score) for hw, score in matched_hotwords if score < 1.0]
+            # 提取热词文本
+            replaced_names = [hw for hw, score in matched_hotwords]
+            console.print(f'    完全匹配：[green4]{", ".join(replaced_names)}')
 
-            if exact_matches:
-                console.print(f'    完全匹配：[green4]{", ".join(exact_matches)}')
-
+        # 2. 显示潜在热词（从上下文热词中排除已替换的）
+        if potential_hotwords:
+            replaced_set = {hw for hw, score in matched_hotwords}
+            potential_matches = [(hw, score) for hw, score in potential_hotwords if hw not in replaced_set]
+            
             if potential_matches:
                 # 格式化潜在匹配列表，显示分数
                 potential_str = ", ".join([f"{hw}({score:.2f})" for hw, score in potential_matches[:5]])
@@ -299,7 +303,7 @@ class ResultProcessor:
                 text,
                 return_result=True,
                 paste=paste,
-                matched_hotwords=correction_result.matched_hotwords
+                matched_hotwords=potential_hotwords  # 传递上下文热词给 LLM
             )
         else:
             await self._text_output.output(text, paste=paste)