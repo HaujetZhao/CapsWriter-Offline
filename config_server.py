b685fed6d24112be9c2a34ac6e3a70744a67091e
Haujet Zhao <1292756898@qq.com>
2026-04-09 09:36:27 +0800

    临时改端口

--- config_server.py
+++ config_server.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # 服务端配置
 class ServerConfig:
     addr = '0.0.0.0'
-    port = '6022'
+    port = '6023'
 
     # 语音模型选择：'qwen_asr', 'fun_asr_nano', 'sensevoice', 'paraformer'
     model_type = 'qwen_asr'
